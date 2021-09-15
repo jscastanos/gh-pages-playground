e4695fcbd0a70216f7e85ff859cc49988226934b
jscastanos <jec.castanos@gmail.com>
2021-09-15 22:08:28 +0800

    remove custom vite react jsx

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,7 @@
-const reactJsx = require("vite-react-jsx");
-
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   core: {
     builder: "storybook-builder-vite",
-  },
-  async viteFinal(config, { configType }) {
-    // customize the Vite config here
-
-    config.plugins.push(reactJsx.default());
-
-    // return the customized config
-    return config;
   },
 };
