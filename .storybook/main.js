a43c40011a8d763019a6688c57c29cf87fa5fd6e
jscastanos <jec.castanos@gmail.com>
2021-09-16 07:42:50 +0800

    new entries

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,22 @@
+const reactJsx = require("vite-react-jsx");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   core: {
     builder: "storybook-builder-vite",
+  },
+  async viteFinal(config, { configType }) {
+    // customize the Vite config here
+    return {
+      ...config,
+      plugins: [...config.plugins, reactJsx.default()],
+      entries: [
+        `${path.relative(
+          config.root,
+          path.resolve(__dirname, "../stories")
+        )}/**/*.stories.@(js|jsx|ts|tsx)`,
+      ],
+    };
   },
 };
